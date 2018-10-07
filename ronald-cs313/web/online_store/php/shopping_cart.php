ff7de4b53b8abc2ca2d422d1ee72403d5257fda3
Ronald Munoz <ronald1195@icloud.com>
2018-10-06 20:31:01 -0600

    Store files

---
+++ ronald-cs313/web/online_store/php/shopping_cart.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../stylesheets/store.css">
+    <title>Welcome!</title>
+</head>
+<body>
+
+<div class="jumbotron">
+    <div class="container text-center">
+        <h1>The unusual store</h1>
+        <p>Nothing you need. All you want.</p>
+    </div>
+</div>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Roanld's Store</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="../../home_page.html">Products</a></li>
+            <li><a href="#">Deals</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <a href="#" class="navbar-right">
+        </a>
+        <a href="#" class="btn btn-default navbar-btn navbar-right">
+            <span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
+        </a>
+
+    </div>
+
+</nav>
+
+
+<footer class="container-fluid text-center">
+    <p>Online Store Copyright</p>
+    <form class="form-inline">Get deals:
+        <input type="email" class="form-control" size="50" placeholder="Email Address">
+        <button type="button" class="btn btn-danger">Sign Up</button>
+    </form>
+</footer>
+
+</body>
+</html>
