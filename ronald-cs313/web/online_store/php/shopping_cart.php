7e1a623c73b772b94c1b2e8c3051e65b6a88bb18
Ronald Munoz <ronald1195@icloud.com>
2018-10-07 23:42:44 -0600

    Store stuff

--- ronald-cs313/web/online_store/php/shopping_cart.php
+++ ronald-cs313/web/online_store/php/shopping_cart.php
@@ -32,7 +32,7 @@
 
 </nav>
 
-<form action="../conformation.php">
+<form action="../checkout.html">
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
             <button type="submit" class="btn btn-default">Proceed to checkout</button>
